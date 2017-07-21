3fab25695ac1b20c0a2a8e3802336dafbef86eaf
Farhad Yusifov <farhadyu8@gmail.com>
2017-07-21 12:54:04 -0400

    unfunushed

---
+++ p10_calculator.py
@@ -0,0 +1,24 @@
+
+
+def add(numa, numb):
+    return numa + numb
+
+
+def subtract(numa, numb):
+    return numa - numb
+
+
+def multiply(numa, numb):
+    return numa * numb
+
+
+def divide(numa, numb):
+    return numa / numb
+
+
+def power(numa, numb):
+    return numa ** numb
+
+
+def add(numa, numb):
+    pass
